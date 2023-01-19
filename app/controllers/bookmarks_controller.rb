3b2e7d06d9c123461f412423e329c81ff68773a3
nikki dekerf <nikkidekerf@gmail.com>
2023-01-19 20:54:12 +0100

    first front-end steps are made

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action :set_list, except: :destroy
+  before_action :set_list, except: %i[destroy]
   before_action :set_bookmark, only: :destroy
 
   def new
